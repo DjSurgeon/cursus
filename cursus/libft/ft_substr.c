2d937f1058fad7e3e0ed4a419ed3b98522348841
Sergio Jimenez De La Cruz <serjimen@c2r10s2.42madrid.com>
2025-01-28 16:32:32 +0100

    Fix norm

--- cursus/libft/ft_substr.c
+++ cursus/libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: serjimen <serjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 15:05:00 by serjimen          #+#    #+#             */
-/*   Updated: 2025/01/28 16:27:06 by serjimen         ###   ########.fr       */
+/*   Updated: 2025/01/28 16:31:42 by serjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 	if (s == NULL)
 		return (NULL);
 	if (start >= ft_strlen(s))
-		return(ft_calloc(1, ft_strlen(s)));
+		return (ft_calloc(1, ft_strlen(s)));
 	new_substring = ft_calloc(1, len + 1);
 	if (new_substring == NULL)
 		return (NULL);
